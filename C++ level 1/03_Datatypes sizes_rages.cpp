3c03cc329d24448df35cc7a631a7026a08d5f78a
Ouamira <anasouamira@gmail.com>
2024-11-21 15:49:01 +0100

    Created By Anas ouamira

---
+++ C++ level 1/03_Datatypes sizes_rages.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ 
+    cout << "================================================================\n";
+    cout << " char Range : (" << CHAR_MIN << " , " << CHAR_MAX << ")\n";
+    cout << " Unsigned char Range : (" << 0 << " , " << UCHAR_MAX << ")\n\n";
+
+    cout << " short int Range : (" << SHRT_MIN << " , " << SHRT_MAX << ")\n";
+    cout << " Unsigned short int Range : (" << 0 << " , " << USHRT_MAX << ")\n\n";
+
+    cout << " int Range : (" << INT_MIN << " , " << INT_MAX << ")\n";
+    cout << " Unsinged int Range : (" << 0 << " , " << UINT_MAX << ")\n";
+    cout << " long int Range : (" << LONG_MIN << " , " << LONG_MAX << ")\n";
+    cout << " Unsigned long int Range : (" << 0 << " , " << ULONG_MAX << ")\n";
+
+    cout << " long long int Range : (" << LLONG_MIN << " , " << LLONG_MAX << ")\n";
+    cout << " Unsigned long long int Range : (" << 0 << " , " << ULLONG_MAX << ")\n\n";
+    
+    cout << " float Range : (" << FLT_MIN << " , " << FLT_MAX << ")\n";
+    cout << " float (Negative) Range : (" << -FLT_MIN << " , " << -FLT_MAX << ")\n\n";
+
+    cout << " double Range : (" << DBL_MIN << " , " << DBL_MAX << ")\n";
+    cout << " double (Negative) Range : (" << -DBL_MIN << " , " << -DBL_MAX << ")\n";
+    cout << " double Range : (" << DBL_MIN << " , " << DBL_MAX << ")\n";
+    cout << " long double Range : (" << LDBL_MIN_10_EXP << " , " << LDBL_MAX_10_EXP << ")\n\n";
+    cout << "================================================================\n";
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
