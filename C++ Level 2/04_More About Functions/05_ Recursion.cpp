9bead53b2ce1b5321427c5d147c6b9158767c062
Ouamira <anasouamira@gmail.com>
2025-04-18 08:50:23 +0100

    Created By Anas Ouamira

--- C++ Level 2/04_More About Functions/05_ Recursion.cpp
+++ C++ Level 2/04_More About Functions/05_ Recursion.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+// Function to calculate 'base' raised to the power 'Power' using recursion
+int MyPower(short base, short Power)
+{
+    // Base case: anything raised to the power of 0 is 1
+    if (Power == 0)
+        return 1;
+    else
+    {
+        // Recursive step: base * base^(Power - 1)
+        return (base * MyPower(base, Power - 1));
+    }
+}
+
+
 // ✅ Recursive function to print numbers from N to M
 void PrintNumbersFromNtoM(int N, int M)
 {
@@ -17,5 +31,6 @@ void PrintNumbersFromNtoM(int N, int M)
 int main() {
     // Main function calls PrintNumbersFromNtoM to print numbers from 1 to 10
     PrintNumbersFromNtoM(1, 10);  // Start recursion from N = 1 to M = 10
+    MyPower(2,4);
     return 0;  // End of the program
 }
